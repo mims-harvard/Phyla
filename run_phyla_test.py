480fb76da7fb1b3004dd63e3ac9df1f8a62261d7
Yasha Ektefaie <10067571+yashaektefaie@users.noreply.github.com>
2025-01-13 23:18:56 -0500

    Update run_phyla_test.py

--- run_phyla_test.py
+++ run_phyla_test.py
@@ -1,7 +1,7 @@
 from phyla import phyla
 
 seq = "./phyla/data/40seqs.fasta"
-model = phyla().load().cuda()
+model = phyla(name='phyla-alpha').load().cuda()
 encoded_aa, cls_token_mask, sequence_mask, sequence_names = model.encode_fasta(seq)
 preds = model(encoded_aa, sequence_mask, cls_token_mask)
 tree = model.reconstruct_tree(preds, sequence_names)
